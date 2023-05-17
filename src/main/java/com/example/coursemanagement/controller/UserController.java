c3a35e0972dc60e14a75afa1b1206a1236d004e8
follow <follow@qq.com>
2023-05-17 11:57:57 +0800

    1

--- src/main/java/com/example/coursemanagement/controller/UserController.java
+++ src/main/java/com/example/coursemanagement/controller/UserController.java
@@ -5,12 +5,16 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.coursemanagement.entity.User;
 import com.example.coursemanagement.response.CommonReturnType;
 import com.example.coursemanagement.service.UserService;
+import com.mysql.cj.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Objects;
 
 /**
  * <p>
@@ -38,11 +42,26 @@ public class UserController {
                         .eq("password", user.getPassword()));
 
         if(one!=null){
-            return CommonReturnType.create(one.getRole(),"success");
+            return CommonReturnType.create(null);
         }
         return CommonReturnType.create(null,"用户名或密码错误");
-
     }
 
+    @PostMapping("/register")
+    public CommonReturnType register(@RequestBody User user){
+        User one = userService.getOne(new QueryWrapper<User>().eq("phone", user.getPhone()));
+        if(one!=null){
+            return CommonReturnType.create(null,"用户名已存在");
+        }
+        else {
+            if(StringUtils.isEmptyOrWhitespaceOnly(one.getPhone()) || StringUtils.isEmptyOrWhitespaceOnly(one.getPassword())){
+                return CommonReturnType.create(null,"用户名或密码不能为空");
+            }
+            else{
+                userService.save(user);
+            }
+        }
+        return CommonReturnType.create(null);
+    }
 }
 
